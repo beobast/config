d5cef9605fc215e7e10303d30be3fe4a256d4e11
Bastien <contact@bastienfaure.fr>
2014-09-12 23:44:55 +0200

    Bug fix

--- install.sh
+++ install.sh
@@ -1,41 +1,36 @@
 #! /bin/sh
 #####################################################
-#  Install personal configs	                    #
+#  Install personal configs	                        #
 #  Author: Bastien Faure <http://bastienfaure.fr>   #
 #                        <contact@bastienfaure.fr>  #
 #####################################################
 
-CWD=$(pwd)
-
 ## Functions
 ask()
 {
-  echo -n "$1  [y/n]"
-  read a
-  while [ "$a" != "y" ] && [ "$a" != "n" ]
+  echo -n "$1  [y/n] "
+  read answer
+  while [ $answer != "y" ] && [ $answer != "n" ]
   do
     echo "Answer \"y\" (yes) or \"n\" (no)"
-    echo -n "$1 [y/n]"
-    read a
+    echo -n "$1 [y/n] "
+    read answer
   done
 }
 
 ## Let's go
-clear
-cd
 ask "Install vimrc ?"
-if [ "$a" = "y" ]; then
-	cp vimrc ~/.vimrc	
+if [ "$answer" = "y" ]; then
+	cp ./vimrc ~/.vimrc	
 fi
 
-clear
 ask "Install terminator config ?"
-if [ "$a" = "y" ]; then
-	cp terminator-config ~/.config/terminator/config
+if [ "$answer" = "y" ]; then
+	cp ./terminator-config ~/.config/terminator/config
 fi
 
-clear
 ask "Install sources.list ?"
-if [ "$a" = "y" ]; then
-	sudo cp $CWD/sources.list /etc/apt/sources.list
+if [ "$answer" = "y" ]; then
+	sudo cp /etc/apt/sources.list /etc/apt/sources.list.old
+	sudo cp ./sources.list /etc/apt/sources.list
 fi
